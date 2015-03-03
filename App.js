b641dc2e48d42e7546ca5010d7fa7927d12a0f5d
Richard Anaya <richard.anaya@gmail.com>
2015-03-03 14:28:38 -0600

    Update App.js

--- App.js
+++ App.js
@@ -4,3 +4,27 @@ document.getElementById('buttonAnalyze').addEventListener('click',function(){
         document.getElementById('resultArea').innerHTML = JSON.stringify(result);
     });
 });
+
+
+//SOME TESTS
+var assertObjects = function(msg,obja,objb){
+    console.assert(JSON.stringify(obja)==JSON.stringify(objb),msg);
+}
+
+var analyzer = new Analyzer()
+var expectedA = {"mentions":["chris"]};
+analyzer.analyze("@chris you around?").then(function(result){
+    assertObjects("Test mentions work",result,expectedA);
+});
+var expectedB = {"emoticons":["megusta","coffee"]};
+analyzer.analyze("Good morning! (megusta) (coffee)").then(function(result){
+    assertObjects("Test emoticons work",result,expectedB);
+});
+var expectedC = {"links":[{"url":"http://www.nbcolympics.com","title":"Lolcats! - Learn more about lolcats"}]};
+analyzer.analyze("Olympics are starting soon; http://www.nbcolympics.com").then(function(result){
+    assertObjects("Test links work",result,expectedC);
+});
+var expectedD = {"emoticons":["success"],"mentions":["bob","john"],"links":[{"url":"https://twitter.com/jdorfman/status/430511497475670016","title":"Lolcats! - Learn more about lolcats"}]};
+analyzer.analyze("@bob @john (success) such a cool feature; https://twitter.com/jdorfman/status/430511497475670016").then(function(result){
+    assertObjects("Test all works",result,expectedD);
+});
