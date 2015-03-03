a5e2dfa5384a92b4a84f7e906af410cda33cd53a
Richard Anaya <richard.anaya@gmail.com>
2015-03-03 13:48:46 -0600

    Update PageHelper.js

--- PageHelper.js
+++ PageHelper.js
@@ -3,19 +3,8 @@ var PageHelper = function(){
 }
 
 PageHelper.getTitleForUrl = function(url){
+    //TODO - do some serverside cross-domain screen scraping to get title
     return Q.Promise(function(resolve, reject, notify) {
-        var shortUrl = url.replace("http://","");
-        shortUrl = shortUrl.replace("https://","");
-        $.ajax({
-              url: "http://textance.herokuapp.com/title/"+shortUrl,
-              complete: function(data) {
-                if(data.status == 200){
-                    resolve(data.responseText);
-                }
-                else {
-                    resolve("");
-                }
-              }
-        });
+         resolve("Lolcats! - Learn more about lolcats");
     });
 }
