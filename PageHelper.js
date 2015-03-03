cc381dd6e04cf68fa183b9470efe61fbec399bae
Richard Anaya <richard.anaya@gmail.com>
2015-03-03 11:32:15 -0600

    Update PageHelper.js

--- PageHelper.js
+++ PageHelper.js
@@ -2,8 +2,9 @@ var PageHelper = function(){
 
 }
 
-PageHelper.getTitleForUrl = function(){
+PageHelper.getTitleForUrl = function(url){
     return Q.Promise(function(resolve, reject, notify) {
+        //TODO: use real backend service to get titles of other pages by url cross-domainly
         resolve("Lolcats! - Learn more about lolcats");
     });
 }
