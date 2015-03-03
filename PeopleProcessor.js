1ac18d22438e08eb83dd9d321d8b396afa7b28eb
Richard Anaya <richard.anaya@gmail.com>
2015-03-03 11:35:00 -0600

    Update PeopleProcessor.js

--- PeopleProcessor.js
+++ PeopleProcessor.js
@@ -10,7 +10,10 @@ PeopleProcessor.prototype.process = function(input,result){
             if(!result.mentions){
                 result.mentions = [];
             }
-            result.mentions.push(match[1]);
+            var name = match[1];
+            if(result.mentions.indexOf(name)==-1){
+                result.mentions.push(name);
+            }
         }
         resolve();
     });
