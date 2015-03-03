d5facaafeada2d42f38a3e4fd1b57acce5d87eb4
Richard Anaya <richard.anaya@gmail.com>
2015-03-03 13:35:43 -0600

    Update PageHelper.js

--- PageHelper.js
+++ PageHelper.js
@@ -5,7 +5,7 @@ var PageHelper = function(){
 PageHelper.getTitleForUrl = function(url){
     return Q.Promise(function(resolve, reject, notify) {
         $.ajax({
-          url: url,
+          url: "http://textance.herokuapp.com/title/"+url,
           async: true,
           success: function(data) {
             var matches = data.match(/<title>(.*?)<\/title>/);
