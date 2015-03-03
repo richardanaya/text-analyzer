f90d921ab2dc3aefca9208f8ca205e345492319b
Richard Anaya <richard.anaya@gmail.com>
2015-03-03 13:25:59 -0600

    Update PageHelper.js

--- PageHelper.js
+++ PageHelper.js
@@ -2,8 +2,15 @@ var PageHelper = function(){
 
 }
 
-PageHelper.getTitleForUrl = function(){
+PageHelper.getTitleForUrl = function(url){
     return Q.Promise(function(resolve, reject, notify) {
-        resolve("Lolcats! - Learn more about lolcats");
+        $.ajax({
+          url: url,
+          async: true,
+          success: function(data) {
+            var matches = data.match(/<title>(.*?)<\/title>/);
+            resolve(matches[0]);
+          }   
+        });
     });
 }
